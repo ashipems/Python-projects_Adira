535bbe59853aa5bbbd70e60cdcf7b95aa468980d
ashipems <53615978+ashipems@users.noreply.github.com>
2021-09-20 13:02:16 +0530

    Create discord.py

---
+++ discord.py
@@ -0,0 +1,39 @@
+import discord
+import random
+
+TOKEN = 'PLACE YOUR TOKEN HERE'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(username + " : " + user_message + " (" + channel + ")")
+
+    if message.author == client.user:
+        return
+        
+    if message.channel.name == "demo_channel":
+        if user_message.lower() == 'hello':
+            await message.channel.send("Hello "+username+"!")
+            return
+
+        elif user_message.lower() == 'bye':
+            await message.channel.send("See you later "+username+"!")
+            return
+
+        elif user_message.lower() == 'random':
+            await message.channel.send("This is your random number: " + str(random.randrange(1000)) + "!")
+            return
+
+    if user_message.lower() == 'anywhere':
+        await message.channel.send("This can be used anywhere!")
+        return
+
+client.run(TOKEN)
